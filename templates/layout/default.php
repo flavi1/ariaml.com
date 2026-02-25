98f86a8a0be5f689bfdca8590276ab15c13fd4b9
Flavi1 <flavien.guillon@gmail.com>
2026-02-25 10:54:56 +0100

    Update default.php

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -37,10 +37,12 @@ unset($queryParams['pass'], $queryParams['_matchedRoute'], $queryParams['_Token'
 
 </head>
 <body>
-	<div class="logo"><img alt="Aria Markup Language" src="https://flavi1.github.io/aria-ml/src/icon/logo.svg"></div>
+	<a href="<?= $this->Url->build('/') ?>" class="logo">
+		<img alt="Aria Markup Language" src="https://flavi1.github.io/aria-ml/src/icon/logo.svg">
+		<div><span>Aria</span>ML</div>
+	</a>
     <nav class="top-nav">
         <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Aria</span>ML</a>
             
 			<div class="lang-switcher">
 				<?php 
